e9066276879abab1cc6fa69440536802b268825a
ryans <ryans@jlab.org>
2018-05-15 15:31:45 -0400

    Still working on initial setup

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var convertRouter = require('./routes/convert');
+var printRouter = require('./routes/print');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/convert', convertRouter);
+app.use('/print', printRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
