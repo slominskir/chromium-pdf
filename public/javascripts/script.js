6a1b364e7cd60dbbbefd309a7bef227014d82a76
ryans <ryans@jlab.org>
2018-05-24 16:01:41 -0400

    Add Parameters

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var formElement = document.getElementById("parameters-form"),
-        previewElement = document.getElementById("preview-div");
+        previewElement = document.getElementById("preview-div"),
+        formatElement = document.getElementById("format"),
+        customElement = document.getElementById("custom");
 
     var updatePreview = function(event){
         var str = $(formElement).serialize();
         previewElement.innerHTML=location.origin.concat('/puppet-show/pdf?').concat(str);
+        
+        if(formatElement.value === 'custom') {
+            customElement.style.display = "table-row";
+        } else {
+            customElement.style.display = "none";
+        }
     };
 
     formElement.addEventListener('change', updatePreview);
