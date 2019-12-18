2b936af2124c0ac6dbead361c26ded68567c0da5
ryans <ryans@jlab.org>
2019-12-18 11:18:54 -0500

    Show version number in app

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
+var pkgInfo = require('../package.json');
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Puppet Show'});
+    res.render('index', {title: 'Puppet Show', version: pkgInfo.version});
 });
 
 module.exports = router;
